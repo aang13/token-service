f48be631966d7dc4fd761c76a27b681bdb20b1cf
moinul <moinul@moinuls-MacBook-Pro.local>
2023-10-06 11:03:16 +0600

    token generator service add

---
+++ validator/src/main/java/com/aang13/validator/ValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.aang13.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorApplication.class, args);
+	}
+
+}

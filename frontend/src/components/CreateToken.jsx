bb60742dfb464dcae378161dcd7779783375e7d0
moinul <mdmoinulh13@gmail.com>
2023-10-09 00:22:57 +0600

    loader added for create token page, validate token page added with all styling, functionality

--- frontend/src/components/CreateToken.jsx
+++ frontend/src/components/CreateToken.jsx
@@ -1,50 +1,71 @@
 import { useState } from 'react'
 import api from '../api/axiosConfig'
+import ValidateToken from './ValidateToken'
 const CreateToken = () => {
 
+    const [isLoading, setIsLoading] = useState(false)
     const [token, setToken] = useState("")
     const [selectedDigits, setSelectedDigits] = useState([])
 
     const handleDigitClick = (digit) => {
         if (selectedDigits.includes(digit)) {
-            setSelectedDigits(selectedDigits.filter((d) => d !== digit));
+            setSelectedDigits(selectedDigits.filter((d) => d !== digit))
         } else {
-            setSelectedDigits([...selectedDigits, digit]);
+            setSelectedDigits([...selectedDigits, digit])
         }
     }
 
     const generate = () => {
-        api.post("http://localhost:8080/generate",{"numberList":selectedDigits})
+        if (selectedDigits.length === 0) {
+            alert('Please select atleast 1 digit!')
+            return
+        }
+        setIsLoading(true)
+        api.post("http://localhost:8001/generate",{"numberList":selectedDigits})
         .then(response=>{
             setToken(response.data)
+        }).catch(error=>{
+            alert("Error creating token!")
+        }).finally(()=>{
+            setIsLoading(false)
+            setSelectedDigits([])
         })
     }
     return(
         <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <a className="navbar-brand mx-auto font-weight-bolder" href="#">
-                    Generate and validate your tokens at one stop!
-                </a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand mx-auto font-weight-bolder" href="#">
+                        Generate and validate your tokens at one stop!
+                    </a>
+                </div>
+            </nav>
+            <div className="container mt-4">
+                <p className="text-center">Please select the digits you want to create the token with:</p>
+                <div className="d-flex justify-content-center">
+                    {Array.from({ length: 10 }, (_, digit) => (
+                        <button
+                            key={digit}
+                            className={`btn btn-outline-primary mx-2 ${selectedDigits.includes(digit) && 'btn-primary'}`}
+                            onClick={() => handleDigitClick(digit)}
+                        >
+                            {digit}
+                        </button>
+                    ))}
+                </div>
             </div>
-        </nav>
-        <div className="container mt-4">
-            <p className="text-center">Please select the digits you want to create the token with:</p>
-            <div className="d-flex justify-content-center">
-                {Array.from({ length: 10 }, (_, digit) => (
-                    <button
-                        key={digit}
-                        className={`btn btn-outline-primary mx-2 ${selectedDigits.includes(digit) && 'btn-primary'}`}
-                        onClick={() => handleDigitClick(digit)}
-                    >
-                        {digit}
+            <div className="container d-flex justify-content-center mt-5 mb-20">
+                {isLoading ? (
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                ) : (
+                    <button className="btn btn-primary" onClick={generate}>
+                        {isLoading ? 'Generating Token...' : 'Generate Token'}
                     </button>
-                ))}
+                )}
             </div>
-        </div>
-        <div className="container d-flex justify-content-center mt-5">
-                <button type="button" className="btn btn-primary" onClick={generate}>Generate Token</button>
-        </div>
+            {token !== '' && <ValidateToken data={token}></ValidateToken>}
         </>
     )
 }
--- frontend/src/components/ValidateToken.jsx
+++ frontend/src/components/ValidateToken.jsx
@@ -1,7 +1,63 @@
-const ValidateToken = () => {
+import { useEffect, useState } from 'react'
+import api from '../api/axiosConfig'
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
+
+const ValidateToken = (props) => {
+    const [token, setToken]= useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [isValid, setIsValid] = useState(false)
+    const [isApiResponse, setIsApiResponse] = useState(false)
+
+    useEffect(() =>{
+        setToken(props.data.token)
+        setIsApiResponse(false)
+    },[props.data.token])
+
+    const handleChange = (e) =>{
+        setToken(e.target.value)
+    }
+
+    const validate =() => {
+        setIsLoading(true)
+        api.get("http://localhost:8002/validator",{params:{token: token}})
+        .then(response=>{
+            setIsValid(response.data.isValid)
+        }).catch(error=>{
+            alert("Error validating token!")
+        }).finally(()=>{
+            setIsLoading(false)
+            setIsApiResponse(true)
+        })
+    }
+
     return (
         <>
-        Hello World!
+            <hr></hr>
+            <div className="container mt-4">
+                <p className="text-center">Generated Token</p>
+                <div className="d-flex justify-content-center">
+                    <input type="text" name="token" value={token} className="form-control" style={{ width: '200px' }} onChange={(e)=>handleChange(e)}/>
+                </div>
+                
+                <div className="d-flex justify-content-center">
+                    {isApiResponse &&
+                        (isValid ? <i className="bi bi-check text-success">Valid</i>
+                        :
+                        <i className="bi bi-x text-danger">Invalid</i>
+                    )}
+                </div>
+                <div className="container d-flex justify-content-center mt-5">
+                {isLoading ? (
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                ) : (
+                    <button className="btn btn-info" onClick={validate}>
+                        {isLoading ? 'Validating Token...' : 'Validate Token'}
+                    </button>
+                )}
+                </div>
+            </div>
         </>
     )
 }
